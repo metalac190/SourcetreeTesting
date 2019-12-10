4a87f380ecf001ed9d886d39c15ff617137af1e8
Adam <achaysmail@gmail.com>
2019-12-10 17:15:34 -0600

    Creating some scripts

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] GameObject object01 = null;
+    [SerializeField] GameObject object02 = null;
+
+}
